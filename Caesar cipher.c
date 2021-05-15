897f56084f5e67172b970196f2c87fe552c1cc35
whitedevil1710 <75324996+whitedevil1710@users.noreply.github.com>
2021-05-15 13:38:49 +0530

    Update Caesar cipher.c

--- Caesar cipher.c
+++ Caesar cipher.c
@@ -1,30 +1,31 @@
 #include<stdio.h>
- 
 int main()
 {
     car text[100], c;
     int i, key;
-    printf("Enter a text to encrypt: ");
+    
+    printf("Enter a text to decrypt: ");
     gets(text);
     printf("Enter key: ");
     scanf("%d", &key);
+    
     for(i = 0; text[i] != '\0'; ++i){
         c = text[i];
         if(c >= 'a' && c <= 'z'){
-            c = c + key;
-            if(c > 'z'){
-                c = c - 'z' + 'a' - 1;
+            c = c - key;
+            if(c < 'a'){
+                c = c + 'z' - 'a' + 1;
             }
             text[i] = c;
         }
         else if(c >= 'A' && c <= 'Z'){
-            c = c + key;
-            if(c > 'Z'){
-                c = c - 'Z' + 'A' - 1;
+            c = c - key;
+            if(c < 'A'){
+                c = c + 'Z' - 'A' + 1;
             }
             text[i] = c;
         }
     }
-    printf("Encrypted text: %s", text);
+    printf("Decrypted text: %s", text);
     return 0;
 }
