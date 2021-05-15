4fa5d11a32e12780f40b18b15cd4387c03cadef0
whitedevil1710 <75324996+whitedevil1710@users.noreply.github.com>
2021-05-15 13:43:57 +0530

    Create Xor.c

---
+++ Xor.c
@@ -0,0 +1,17 @@
+include <stdio.h>
+#include <string.h>
+int main()
+{
+ char key[10],string[100];
+ printf("String: ");
+ fgets(string,sizeof(string),stdin);
+ printf("Single Character: ");
+ fgets(key,sizeof(key),stdin);
+ int len=strlen(string);
+ for (int n=0;n<len;++n)
+ {
+  string[n]=string[n]^key[n%strlen(key)];
+ }
+ printf("%s",string);
+ return 0;
+}
